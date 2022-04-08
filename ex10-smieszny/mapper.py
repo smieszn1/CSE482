8b5553a3664360c4cfb84b65e3687061cd0fb23a
Ubuntu <ubuntu@ip-172-31-18-113.ec2.internal>
2022-04-08 20:12:53 +0000

    ex10 result

--- ex10-smieszny/mapper.py
+++ ex10-smieszny/mapper.py
@@ -2,10 +2,12 @@
 
 import sys
 
+counties = ["Clinton","Eaton","Ingham","Kent","Macomb","Shiawassee","Wayne"]
+
 for line in sys.stdin:
     values = line.split(',')
-    key = values[0]
     for i in range(1, len(values)):
+        key = counties[i-1]
         num = ''
         for char in values[i]:
             if char.isdigit():
